666de333a765b946dd9aca25809ced284b9ed21e
Roberto <robertogaliz@gmail.com>
2023-01-05 20:31:45 -0300

    actualizo

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,6 +8,18 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  class Usuario {
+    constructor(opciones) {
+      this.usuario = opciones.usuario,
+      this.nombre = opciones.nombre,
+      this.email = opciones.email,
+      this.password = opciones.password 
+    }
+    saludar() {
+      return "Hola, mi nombre es " + this.nombre;
+    }
+  }
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
